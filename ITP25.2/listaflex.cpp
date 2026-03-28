f16bcb86051da308768158da9396a1f7318b173c
lorenmntlla <131608199+lorenmntlla@users.noreply.github.com>
2026-03-28 14:48:13 -0300

    Refactors listaflex.cpp by removing an array and for loop.

--- ITP25.2/listaflex.cpp
+++ ITP25.2/listaflex.cpp
@@ -6,13 +6,14 @@ int main(void) {
     cin >> numItens >> numCompras;
 
     int itens[numItens];
-    int compras[numCompras];
 
     for (int i = 0; i < numItens; i++) cin >> itens[i];
-    for (int i = 0; i < numCompras; i++) cin >> compras[i];
 
     int total = 0;
-    for (int i = 0; i < numCompras; i++) total += itens[compras[i] - 1];
+    for (int compra, i = 0; i < numCompras; i++) { 
+        cin >> compra;
+        total += itens[compra - 1];
+    }
 
     cout << total << '\n';
 }
