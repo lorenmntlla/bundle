b96359b9ab6d15f29ee953cacd6510a29806b810
lorenmntlla <131608199+lorenmntlla@users.noreply.github.com>
2026-04-01 21:01:46 -0300

    Declares seq inside for initializer, as its never needed out of it

--- lanche.cpp
+++ lanche.cpp
@@ -8,10 +8,8 @@ int main(void) {
     int receitas[sizeReceitas];
     for (int i = 0; i < sizeReceitas; i++) cin >> receitas[i];
     
-    int seq;
-    
     int plates = 0;
-    for (int i = 0, j = 0; j < sizeReceitas && i < sizeSeq; i++) {
+    for (int seq, i = 0, j = 0; j < sizeReceitas && i < sizeSeq; i++) {
         cin >> seq;
 
         j += receitas[j] == seq ? 1 : 0;
